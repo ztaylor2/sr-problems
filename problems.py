64f1be7d5fe4d525e427a0999a615d0185b8c7f3
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-04-18 14:33:00 -0700

    wrote out problems, ready to solve them.

---
+++ problems.py
@@ -0,0 +1,10 @@
+"""Problem solutions.
+
+1. Implement a function to check if a binary tree is balanced. For the purposes of this question,
+a balanced tree is defined to be a tree such that the heights of the two subtrees of any node
+never differ by more than one.
+2. Implement an algorithm to find the kth to last element of a singly linked list.
+3. Write a program that outputs all possibilities to put the operators ‘+’, ‘-’, or nothing between
+the numbers 1,2,...,9 (in this order) such that the result is 100. For example 1 + 2 + 3 - 4 + 5
++ 6 + 78 + 9 = 100.
+"""
