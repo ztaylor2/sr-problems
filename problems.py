e506cf626537f2d57cb239229499a169eb7138ef
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-04-18 14:42:36 -0700

    added solution for determining if a bst is balanced.

--- problems.py
+++ problems.py
@@ -8,3 +8,26 @@ never differ by more than one.
 the numbers 1,2,...,9 (in this order) such that the result is 100. For example 1 + 2 + 3 - 4 + 5
 + 6 + 78 + 9 = 100.
 """
+
+
+"""1. Check if a bst is balanced at every node."""
+
+
+def is_balanced(root):
+    """Check if balanced."""
+    return is_balanced_int(root) >= 0
+
+
+def is_balanced_int(root):
+    """Check if balanced."""
+    if root is None:
+        return 0
+    left = is_balanced_int(root.left)
+    right = is_balanced_int(root.right)
+
+    if left < 0 or right < 0 or abs(left - right) > 1:
+        return -1
+    return max((left, right)) + 1
+
+"""2. Implement an algorithm to find the kth to last element of a singly linked list."""
+
